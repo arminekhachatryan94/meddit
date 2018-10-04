b81622307a5429682cdc2f91029b70784e4b9d3a
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-10-03 21:02:47 -0700

    use correct http status codes

--- forum/app/Services/PostService.php
+++ forum/app/Services/PostService.php
@@ -28,13 +28,13 @@ class PostService implements PostContract {
             if( count($errors) ) {
                 return response()->json([
                     'errors' => $errors
-                ], 401);
+                ], 400);
             } else {
                 if( $post->user_id == $postData['user_id'] ){
                     $post->title = $postData['title'];
                     $post->body = $postData['body'];
                     $post->save();
-                    return response()->json(['post' => $post], 201);
+                    return response()->json(['post' => $post], 200);
                 } else {
                     return response()->json(['errors' => ['invalid' => 'You do not have permission to edit this post']], 401);
                 }
@@ -44,7 +44,7 @@ class PostService implements PostContract {
                 'errors' => [
                     'invalid' => 'Post not found'
                 ]
-            ], 401);
+            ], 404);
         }
     }
 
