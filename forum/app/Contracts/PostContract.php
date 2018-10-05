f0d3080cc03a5bcd559d29cc28a1015727eaccdc
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-10-04 23:09:00 -0700

    refactor edit post and get all posts

--- forum/app/Contracts/PostContract.php
+++ forum/app/Contracts/PostContract.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Contracts;
+use Illuminate\Http\Request;
 use App\Post;
 
 interface PostContract{
     public function getPost($id);
     public function getAllPosts();
-    public function editPost($postData, $id);
+    public function editPost(Post $post, $req);
     public function deletePost(Post $post);
 }
 ?>
