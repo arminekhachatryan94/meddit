d54c24cbcc8f0961b4199e2228f21fab2925d180
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-10-23 23:46:59 -0700

    test get users except

--- forum/tests/Unit/Services/UserServiceTest.php
+++ forum/tests/Unit/Services/UserServiceTest.php
@@ -129,4 +129,55 @@ class UserServiceTest extends TestCase
             $this->assertEquals($user->updated_at, $getUser->updated_at);
         }
     }
+
+    public function test_get_users_except()
+    {
+        $userService = new UserService();
+
+        $user = factory(User::class, 1)->create()->first();
+        $users = factory(User::class, 5)->create();
+
+        $getUsers = $userService->getUsersExcept($user->id);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'username' => $user->username,
+            'password' => $user->password,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at
+        ]);
+
+        for( $i = 0; $i < count($users); $i++ ){
+            $this->assertDatabaseHas('users', [
+                'id' => $users[$i]->id,
+                'first_name' => $users[$i]->first_name,
+                'last_name' => $users[$i]->last_name,
+                'email' => $users[$i]->email,
+                'username' => $users[$i]->username,
+                'password' => $users[$i]->password,
+                'created_at' => $users[$i]->created_at,
+                'updated_at' => $users[$i]->updated_at
+            ]);
+
+            $this->assertEquals($users[$i]->id, $getUsers[$i]->id);
+            $this->assertEquals($users[$i]->first_name, $getUsers[$i]->first_name);
+            $this->assertEquals($users[$i]->last_name, $getUsers[$i]->last_name);
+            $this->assertEquals($users[$i]->email, $getUsers[$i]->email);
+            $this->assertEquals($users[$i]->username, $getUsers[$i]->username);
+            $this->assertEquals($users[$i]->password, $getUsers[$i]->password);
+            $this->assertEquals($users[$i]->created_at, $getUsers[$i]->created_at);
+            $this->assertEquals($users[$i]->updated_at, $getUsers[$i]->updated_at);
+
+            $this->assertFalse($user->id == $getUsers[$i]->id);
+            $this->assertFalse($user->first_name == $getUsers[$i]->first_name);
+            $this->assertFalse($user->last_name == $getUsers[$i]->last_name);
+            $this->assertFalse($user->email == $getUsers[$i]->email);
+            $this->assertFalse($user->username == $getUsers[$i]->username);
+            $this->assertFalse($user->password == $getUsers[$i]->password);
+            $this->assertFalse($user->created_at == $getUsers[$i]->created_at);
+        }
+    }
 }
