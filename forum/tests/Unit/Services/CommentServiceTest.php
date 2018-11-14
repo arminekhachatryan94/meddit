f7cdf7d0d2f444a8128b0c6c5eaf672f69f30078
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-13 22:18:18 -0800

    test get comment

--- forum/tests/Unit/Services/CommentServiceTest.php
+++ forum/tests/Unit/Services/CommentServiceTest.php
@@ -22,7 +22,64 @@ class CommentServiceTest extends TestCase
     }
 
     /**
-     * Create a comment.
+     * Test get comment
+     * 
+     * @test
+     */
+    public function test_get_comment()
+    {
+        $user = factory(User::class, 1)->create()->first();
+        $posts = factory(Post::class, 10)->create(['user_id' => $user->id]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'username' => $user->username,
+            'password' => $user->password,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at
+        ]);
+
+        foreach( $posts as $post ){
+            $this->assertDatabaseHas('posts', [
+                'id' => $post->id,
+                'user_id' => $post->user_id,
+                'title' => $post->title,
+                'body' => $post->body,
+                'created_at' => $post->created_at,
+                'updated_at' => $post->updated_at
+            ]);
+
+            $comments = factory(Comment::class, 10)->create(['user_id' => $user->id]);
+
+            foreach( $comments as $comment ){
+                $this->assertDatabaseHas('comments', [
+                    'id' => $comment->id,
+                    'user_id' => $comment->user_id,
+                    'post_id' => $comment->post_id,
+                    'comment_id' => $comment->comment_id,
+                    'body' => $comment->body,
+                    'created_at' => $comment->created_at,
+                    'updated_at' => $comment->updated_at
+                ]);
+
+                $get_comment = $this->commentService->getComment($comment->id);
+                $this->assertEquals($get_comment->id, $comment->id);
+                $this->assertEquals($get_comment->user_id, $comment->user_id);
+                $this->assertEquals($get_comment->post_id, $comment->post_id);
+                $this->assertEquals($get_comment->comment_id, $comment->comment_id);
+                $this->assertEquals($get_comment->body, $comment->body);
+                $this->assertEquals($get_comment->created_at, $comment->created_at);
+                $this->assertEquals($get_comment->updated_at, $comment->updated_at);
+
+            }
+        }
+    }
+
+    /**
+     * Test create comment.
      *
      * @test
      */
