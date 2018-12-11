86d27ae3271e181724dbaa4688064e29c7d29cbe
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-12-10 18:39:59 -0800

    test save biography

--- forum/tests/Unit/Services/BiographyServiceTest.php
+++ forum/tests/Unit/Services/BiographyServiceTest.php
@@ -7,6 +7,7 @@ use App\Services\BiographyService;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Biography;
 use App\User;
+use Faker\Factory;
 
 class BiographyServiceTest extends TestCase
 {
@@ -57,7 +58,7 @@ class BiographyServiceTest extends TestCase
      * Test get biography
      * 
      * @test
-    */
+     */
     public function test_get_biography() {
         for( $i = 0; $i < 10; $i++ ){
             $user = factory(User::class, 1)->create()->first();
@@ -90,7 +91,50 @@ class BiographyServiceTest extends TestCase
             $this->assertEquals($biography->description, $get_bio->description);
             $this->assertEquals($biography->created_at, $get_bio->created_at);
             $this->assertEquals($biography->updated_at, $get_bio->updated_at);
+        }
+    }
 
+    /**
+     * Test save biography
+     * 
+     * @test
+     */
+    public function test_save_biography() {
+        for( $i = 0; $i < 10; $i++ ){
+            $faker = Factory::create();
+            $user = factory(User::class, 1)->create()->first();
+
+            $this->assertDatabaseHas('users', [
+                'id' => $user->id,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'email' => $user->email,
+                'username' => $user->username,
+                'password' => $user->password,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at
+            ]);
+
+            $biography = factory(Biography::class, 1)->create(['user_id' => $user->id])->first();
+
+            $this->assertDatabaseHas('biographies', [
+                'id' => $biography->id,
+                'user_id' => $biography->user_id,
+                'description' => $biography->description,
+                'created_at' => $biography->created_at,
+                'updated_at' => $biography->updated_at
+            ]);
+
+            $description = $faker->sentence;
+            $this->biographyService->saveBiography($biography, $description);
+
+            $this->assertDatabaseHas('biographies', [
+                'id' => $biography->id,
+                'user_id' => $biography->user_id,
+                'description' => $description,
+                'created_at' => $biography->created_at,
+                'updated_at' => $biography->updated_at
+            ]);
         }
     }
 }
