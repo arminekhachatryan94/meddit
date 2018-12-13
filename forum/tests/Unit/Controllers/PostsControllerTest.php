e4301f62a242610445557af6300e9b2964cd5a3d
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-12-12 22:31:57 -0800

    test posts on posts controller

--- forum/tests/Unit/Controllers/PostsControllerTest.php
+++ forum/tests/Unit/Controllers/PostsControllerTest.php
@@ -3,12 +3,15 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use App\Http\Controllers\PostsController;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\Request;
+use App\Http\Controllers\PostsController;
 use App\Contracts\PostContract;
 use App\Contracts\UserContract;
 use App\Post;
 use App\User;
+use App\Biography;
+use App\UserRole;
 use Mockery;
 
 class PostControllerTest extends TestCase
@@ -35,6 +38,9 @@ class PostControllerTest extends TestCase
     public function test_get_all_posts()
     {
         $user = factory(User::class, 1)->create()->first();
+        $bio = factory(Biography::class, 1)->create(['user_id' => $user->id])->first();
+        $role = factory(UserRole::class, 1)->create(['user_id' => $user->id])->first();
+
         $this->assertDatabaseHas('users', [
             'id' => $user->id,
             'first_name' => $user->first_name,
@@ -46,7 +52,7 @@ class PostControllerTest extends TestCase
             'updated_at' => $user->updated_at
         ]);
 
-        $factory_posts = factory(Post::class, 10)->create(['user_id' => $user->id]);
+        $factory_posts = factory(Post::class, 1)->create(['user_id' => $user->id]);
         foreach($factory_posts as $post) {
             $this->assertDatabaseHas('posts', [
                 'id' => $post->id,
