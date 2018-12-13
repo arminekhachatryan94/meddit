f9f94e499d43712fce38b1d11f876839db665a04
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-12-12 17:59:09 -0800

    test get user role

--- forum/tests/Unit/Services/UserRoleServiceTest.php
+++ forum/tests/Unit/Services/UserRoleServiceTest.php
@@ -53,4 +53,45 @@ class UserRoleServiceTest extends TestCase
             ]);
         }
     }
+
+    /**
+     * Test create user role
+     * 
+     * @test
+     */
+    public function test_get_user_role()
+    {
+        for( $i = 0; $i < 10; $i++ ){
+            $user = factory(User::class, 1)->create()->first();
+
+            $this->assertDatabaseHas('users', [
+                'id' => $user->id,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'email' => $user->email,
+                'username' => $user->username,
+                'password' => $user->password,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at
+            ]);
+
+            $role = factory(UserRole::class, 1)->create(['user_id' => $user->id])->first();
+
+            $this->assertDatabaseHas('user_roles', [
+                'id' => $role->id,
+                'user_id' => $role->user_id,
+                'role' => $role->role,
+                'created_at' => $role->created_at,
+                'updated_at' => $role->updated_at
+            ]);
+
+            $userrole = $this->userRoleService->getUserRole($user->id);
+
+            $this->assertEquals($role->id, $userrole->id);
+            $this->assertEquals($role->user_id, $userrole->id);
+            $this->assertEquals($role->role, $userrole->role);
+            $this->assertEquals($role->created_at, $userrole->created_at);
+            $this->assertEquals($role->updated_at, $userrole->updated_at);
+        }
+    }
 }
