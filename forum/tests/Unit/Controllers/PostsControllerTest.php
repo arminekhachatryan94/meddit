7f73809b5612648cb984339ea048bd0ed57f7b8b
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-12-14 22:32:23 -0800

    test comments on posts when getting all posts

--- forum/tests/Unit/Controllers/PostsControllerTest.php
+++ forum/tests/Unit/Controllers/PostsControllerTest.php
@@ -12,6 +12,7 @@ use App\Contracts\PostContract;
 use App\Contracts\UserContract;
 use App\Post;
 use App\User;
+use App\Comment;
 use App\Biography;
 use App\UserRole;
 use Mockery;
@@ -32,6 +33,129 @@ class PostControllerTest extends TestCase
         $this->userService = Mockery::spy(UserContract::class);
         $this->postsController = new PostsController($this->postService, $this->userService);
         $this->faker = Faker\Factory::create();
+    }
+
+    private function generateComments(Comment & $comment, int & $level){
+        $rand = rand(0,1);
+        $comment->comments = array();
+        if($rand && $level < 5) {
+            $num = rand(1, 5);
+            $level++;
+            for($j = 0; $j < $num; $j++) {
+                $new_comment = factory(Comment::class, 1)->create(['user_id' => $comment->user_id, 'comment_id' => $comment->id])->first();
+                $this->generateComments($new_comment, $level);
+                $comment->comments = array_merge([$new_comment], $comment->comments);
+            }
+        }
+    }
+
+    public function testComments(Comment & $factory_comment, $comment) {
+        $this->assertEquals($factory_comment->id, $comment->id);
+        $this->assertEquals($factory_comment->user_id, $comment->user_id);
+        $this->assertEquals($factory_comment->post_id, $comment->post_id);
+        $this->assertEquals($factory_comment->body, $comment->body);
+        $this->assertEquals($factory_comment->created_at, $comment->created_at);
+        $this->assertEquals($factory_comment->updated_at, $comment->updated_at);
+
+        $this->assertEquals(count($factory_comment->comments), count($comment->comments));
+        if(count($comment->comments) > 0) {
+            $factoryComments = $factory_comment->comments;
+            $callComments = $comment->comments;
+            usort($factoryComments, function($a, $b) {
+                return strtotime($a->created_at) < strtotime($b->created_at);
+            });
+            for($c = 0; $c < count($callComments); $c++) {
+                $this->testComments($factoryComments[$c], $callComments[$c]);
+            }
+        }
+    }
+
+    /**
+     * Test get all posts with comments.
+     *
+     * @test
+     */
+    public function test_get_all_posts_with_comments()
+    {
+        $user = factory(User::class, 1)->create()->first();
+        $bio = factory(Biography::class, 1)->create(['user_id' => $user->id])->first();
+        $role = factory(UserRole::class, 1)->create(['user_id' => $user->id])->first();
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'username' => $user->username,
+            'password' => $user->password,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at
+        ]);
+
+        $factory_posts = factory(Post::class, 10)->create(['user_id' => $user->id]);
+        $factory_posts = json_decode($factory_posts);
+        usort($factory_posts, function($a, $b) {
+            return strtotime($a->created_at) < strtotime($b->created_at);
+        });
+
+        foreach($factory_posts as $post) {
+            $this->assertDatabaseHas('posts', [
+                'id' => $post->id,
+                'user_id' => $post->user_id,
+                'title' => $post->title,
+                'body' => $post->body,
+                'created_at' => $post->created_at,
+                'updated_at' => $post->updated_at
+            ]);
+            $post->comments = array();
+
+            // create comments
+            for($i = 1; $i <= 10; $i++) {
+                $comment = factory(Comment::class, 1)->create(['user_id' => $user->id, 'post_id' => $post->id])->first();
+                $level = 0;
+                $this->generateComments($comment, $level);
+                array_push($post->comments, $comment);
+            }
+        }
+
+        $response = $this->call('GET', '/api/posts');
+        $response->assertStatus(200);
+
+        $posts = json_decode($response->content())->posts;
+
+        $this->assertEquals(count($factory_posts), count($posts));
+
+        for($i = 0; $i < count($posts); $i++) {
+            $this->assertEquals($factory_posts[$i]->id, $posts[$i]->id);
+            $this->assertEquals($factory_posts[$i]->user_id, $posts[$i]->user_id);
+            $this->assertEquals($factory_posts[$i]->title, $posts[$i]->title);
+            $this->assertEquals($factory_posts[$i]->body, $posts[$i]->body);
+            $this->assertEquals($factory_posts[$i]->created_at, $posts[$i]->created_at);
+            $this->assertEquals($factory_posts[$i]->updated_at, $posts[$i]->updated_at);
+
+            $post_user = $posts[$i]->user;
+            $this->assertEquals($post_user->id, $user->id);
+            $this->assertEquals($post_user->first_name, $user->first_name);
+            $this->assertEquals($post_user->last_name, $user->last_name);
+            $this->assertEquals($post_user->email, $user->email);
+            $this->assertEquals($post_user->username, $user->username);
+            $this->assertEquals($post_user->created_at, $user->created_at);
+            $this->assertEquals($post_user->updated_at, $user->updated_at);
+
+            $factory_comments = $factory_posts[$i]->comments;
+            $comments = $posts[$i]->comments;
+
+            usort($factory_comments, function($a, $b) {
+                return strtotime($a->created_at) > strtotime($b->created_at);
+            });
+
+            $this->assertEquals(count($factory_comments), count($comments));
+
+            // test comments
+            for($j = 0; $j < count($posts[$i]->comments); $j++){
+                $this->testComments($factory_comments[$j], $comments[$j]);
+            }
+        }
     }
 
     /**
